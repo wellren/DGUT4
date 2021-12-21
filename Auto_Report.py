c68fe9efe063e74b4dfb75335d99201c91bcb652
bertramoon <bertramoon@126.com>
2021-12-21 19:59:49 +0800

    v2021-12-21

--- Auto_Report.py
+++ Auto_Report.py
@@ -1,35 +1,57 @@
+from re import S
 from gevent import monkey;monkey.patch_all()
 import click
-from dgut_requests.dgut import dgutIllness
+from dgut_requests.dgut import dgutIllness, requests
 from retry import retry
 import gevent
+import sys
 
 
 @retry(tries=50, delay=2, backoff=2, max_delay=30)
-def clock(u: dgutIllness, location: "list | None" = None) -> None:
+def clock(u: dgutIllness, key: str=None) -> None:
     '''
     打卡并输出结果
     :param u: dgutIllness, 指定打卡的dgutIllness对象.
     :param location: list | None, 指定打卡的定位（经纬度列表类型）或缺省None.
     :returns: None，打印结果
     '''
-    print(u.username[-2:], '-', u.report())
+    result = u.report().get("message")
+    print(u.username[-2:], '-', result) 
+    if key:
+        headers = {
+            'Content-type': "application/x-www-form-urlencoded"
+        }
+        data = {
+            "text": u.username + "打卡成功",
+            "desp": result
+        }
+        res = requests.post(f'https://sctapi.ftqq.com/{key}.send', data=data, headers=headers)
+        if res.status_code == 200:
+            print(u.username[-2:]+"推送成功")
+
+
+
 
 
 @click.command()
 @click.option('-U', '--username', required=True, help="中央认证账号用户名", type=str)
 @click.option('-P', '--password', required=True, help="中央认证账号密码", type=str)
-# @click.option('-L', '--location', help="经纬度")
-def main(username, password):
+@click.option('-K', '--key', help="server酱的key值", type=str)
+def main(username, password, key):
     users = username.split(",")
     pwds = password.split(",")
+    if key:
+        keys = key.split(",")
     # locations = {int(item.split(',')[0]): (float(item.split(',')[1]), float(item.split(
     #     ',')[2])) for item in location.strip('[] ').split('],[')} if location else {}
     if len(users) != len(pwds):
         exit("账号和密码个数不一致")
     tasks = []
-    for usr in zip(users, pwds):
-        tasks.append(gevent.spawn(clock, u=dgutIllness(usr[0], usr[1])))
+    for i in range(len(users)):
+        if key and i < len(keys) and keys[i] != '0':
+            tasks.append(gevent.spawn(clock, u=dgutIllness(users[i], pwds[i]), key=keys[i]))
+        else:
+            tasks.append(gevent.spawn(clock, u=dgutIllness(users[i], pwds[i])))
     # for usr in enumerate(zip(users, pwds), 1):
         # u = dgutIllness(usr[1][0], usr[1][1])
         # tasks.append(gevent.spawn(clock, u=u, location=locations.get(usr[0])))
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # 东莞理工疫情自动打卡助手
 
 ### 使用方法
-设置好Secrets即可。设置方法[点击这里](https://gitee.com/bertramoon/dgut-autoreport-configure)。有多个账号的话，以","分隔开，如设置USERNAME为"2018001,2018002"，PASSWORD为"123456,7891011"。
+设置好Secrets即可。设置方法[点击这里](https://gitee.com/bertramoon/dgut-autoreport-configure)。有多个账号的话，以","分隔开，如设置USERNAME为"2018001,2018002"，PASSWORD为"123456,7891011",SERVER_KEY为"SCT105680T2xabydNVRuTkcMm7PKQgWaJE,SCT105696T3sabwdNBRuTkcMm7XQSgNsCD"。
 
 
 ### 更新日志
 
-- 2021-12-21 - 疫情打卡系统更新，调用的dgut-requests也进行了更新，主要是删除了location参数
+#### 2021-12-21
+- 增加Server酱订阅微信通知服务
+
+#### 2021-12-21
+- 疫情打卡系统更新，调用的dgut-requests也进行了更新，主要是删除了location参数
